837c1c420900928a12542730cd5684eb24021dda
Chris Bond <bondcb@bc.edu>
2022-02-07 17:37:27 -0500

    Week 3: practice

--- Reeses/ViewController.swift
+++ Reeses/ViewController.swift
@@ -9,11 +9,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var messageLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        messageLabel.text = ""
     }
-
-
+    @IBAction func heyButtonPressed(_ sender: UIButton) {
+        let phrase1 = "You Got chocolate in my peanut butter"
+        let phrase2 = "You got peanut butter in my chocolate"
+        
+        if messageLabel.text == phrase1 {
+            messageLabel.text = phrase2
+            messageLabel.textColor = .brown
+            messageLabel.textAlignment = .left
+            } else {
+                messageLabel.text = phrase1
+                messageLabel.textColor = .orange
+                messageLabel.textAlignment = .right
+            }
+        }
 }
 
